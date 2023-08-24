883e46e704c27b906ec59c9a8ad8e5d8c5ab9e2b
AntonShiva <ryashinanthon@yandex.ru>
2023-08-24 20:41:00 +0300

    добавил логику для кнопок + - * / и =

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     // аутлет верней строки
     @IBOutlet weak var displayResultLabel: UILabel!
     
     
     // переменная для первого операнда
     var firstOperand: Double = 0
+    // переменная для второго операнда
+    var secondOperand: Double = 0
+    // переменная знака производящего дейсвие с операндами
+    var operationSing: String = ""
     
     // переменная трансформирующая формат text из верхней строки displayReasultLabel в Double
     var currentInput: Double {
@@ -26,9 +31,8 @@ class ViewController: UIViewController {
             stillTyping = false
         }
     }
-    
     // переменная определяющая в теле nemberPressed порядок ветвления if
-    // когда она false, то ноль заменяеться на введенное значение
+    // когда она false, то в if ноль заменяеться на введенное значение
     var stillTyping = false
     // кнопки цифр
     @IBAction func numberPressed(_ sender: UIButton) {
@@ -45,14 +49,36 @@ class ViewController: UIViewController {
             stillTyping = true
         }
     }
-    
-    
     @IBAction func twoOperandsSignPressed(_ sender: UIButton) {
-      
+        operationSing = sender.currentTitle!
         firstOperand = currentInput
+        stillTyping = false
     }
-    
-    
+    // универсальная функция принимающая клоужеры из equalitySingPresset из кейсов в свиче
+    func operateWithTwoOperands(operation: (Double, Double) -> Double) {
+        currentInput = operation(firstOperand, secondOperand)
+        stillTyping = false
+    }
+    // кнопка = (равно)
+    @IBAction func equalitySignPresset(_ sender: UIButton) {
+        // передаем во второй опернад значение
+        if stillTyping {
+            secondOperand = currentInput
+        }
+        
+        switch operationSing {
+           case "+":
+            operateWithTwoOperands{$0 + $1}
+           case "-":
+            operateWithTwoOperands{$0 - $1}
+           case "×":
+            operateWithTwoOperands{$0 * $1}
+           case "÷":
+            operateWithTwoOperands{$0 / $1}
+        default: break
+        }
+        
+    }
     
         
     
